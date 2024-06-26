74c88f48e07ccbce1736e4e4ce42643994c76dc7
pritamstech <pritamstech@hotmail.com>
2024-06-26 11:53:37 +0530

    Added AI Integration

--- index.js
+++ index.js
@@ -4,13 +4,39 @@ const express = require("express");
 env.config();
 const app = express();
 const port = process.env.PORT || 3000;
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+const chatSession = model.startChat({
+  generationConfig,
+  history: [],
+});
 
 const token = process.env.BOT_API_KEY;
 const bot = new TelegramBot(token, { polling: true });
-bot.on("message", (msg) => {
+bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
-  const messageText = msg.text;
-  bot.sendMessage(chatId, "Welcome to the bot!");
+  // const messageText = msg.text;
+  const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
+  bot.sendMessage(chatId, result.response.text());
   // Process the incoming message here
 });
 app.get("/", (req, res) => {
