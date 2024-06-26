1f3eff7bff0f78799b4b03032adb083ee74ecdb4
pritamstech <pritamstech@hotmail.com>
2024-06-26 11:39:22 +0530

    Pushed With Added Express.js Code

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const TelegramBot = require("node-telegram-bot-api");
 const env = require("dotenv");
+const express = require("express");
 env.config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
 const token = process.env.BOT_API_KEY;
 const bot = new TelegramBot(token, { polling: true });
 bot.on("message", (msg) => {
@@ -8,4 +13,11 @@ bot.on("message", (msg) => {
   const messageText = msg.text;
   bot.sendMessage(chatId, "Welcome to the bot!");
   // Process the incoming message here
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Telegram Bot Is Up And Running ${port}`);
 });
